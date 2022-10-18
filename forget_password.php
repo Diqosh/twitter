d36784a9fa647aa2b471ce6f3ae6cc3bd1da0002
thebackender <free3301@protonmail.com>
2022-10-18 20:54:28 +0600

    logout, forgot password

---
+++ forget_password.php
@@ -0,0 +1,14 @@
+<?php
+    include 'header.php';
+?>
+<div class="form">
+    <div class="close"><i class="fa-solid fa-xmark"></i></div>
+    <div class="forgot">
+        <div class="logo">
+            <i class="fa-brands fa-twitter"></i>
+        </div>
+        <p>Forgot password</p>
+        <input type="text" placeholder="Enter your email"><br>
+        <button>Next</button>
+    </div>
+</div>
---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+    include 'header.php';
+?>
+<div class="form">
+    <div class="forgot">
+        <div class="logo">
+            <i class="fa-brands fa-twitter"></i>
+        </div>
+        <p>Logout of Twitter?</p>
+        <h6>You can always log back in at any time. If you just want to switch accounts, you can do that by adding an existing account. </h6>
+        <button>Log out</button><br>
+        <button class="cancel">Cancel</button>
+    </div>
+</div>
