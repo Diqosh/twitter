a0d51e57aa681e371b940f367746f5fc8086dc68
Dinmukhammed Tleuzhanuly <tdimasg@gmail.com>
2022-10-18 16:14:38 +0600

    part home

---
+++ pages/home.php
@@ -0,0 +1,21 @@
+<div class="news">
+    <div class="news__header">
+        popular image
+    </div>
+    <div class="news__body">
+        <div class="news__creator-avatar">
+        </div>
+        <div class="news__content">
+            <div class="content__header">
+                <div class="content__title">wild TikTok screenshots</div>
+                <div class="content__option"><i class="fa-solid fa-ellipsis"></i></div>
+            </div>
+            <div class="content__image">
+
+            </div>
+        </div>
+    </div>
+    <div class="news__header">
+        buttons
+    </div>
+</div>
