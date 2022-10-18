53b8c9d76063c48b0c9dba37669b0e41b3948579
thebackender <free3301@protonmail.com>
2022-10-18 18:58:52 +0600

    register.php

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php
+    include 'header.php';
+?>
+<div class="register">
+    <div class="poster">
+    </div>
+    <div class="content">
+        <i class="fa-brands fa-twitter"></i>
+        <h1>Happening now</h1>
+        <h3>Join Twitter today.</h3>
+        <button>Sign up with phone or email</button>
+        <p>By signing up, you agree to the <a href="#">Terms of Service</a>  and <a href="#">Privacy Policy</a>, including <a href="#">Cookie Use</a>.</p>
+        <h5>Already have an account?</h5>
+        <button class="signin">Sign in</button>
+    </div>
+</div>
