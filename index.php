fe534c33372e3418b5299bf6fafc1e8ffabc4a24
thebackender <free3301@protonmail.com>
2022-10-18 16:31:57 +0600

    constructed index.php

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
     include 'header.php';
 ?>
-<div style="width: 33%"><?php
-
-    //    include 'left-sidebar.php';
-    //    include 'right-sidebar.php';
-    include 'pages/home.php'
-    ?></div>
+<div class="sim">
+    <section class="leftbar">
+        <?php
+            include 'left-sidebar.php';
+        ?>
+    </section>
+    <section class="main">
+        <?php
+            include 'pages/home.php';
+        ?>
+    </section>
+    <section class="rightbar">
+        <?php
+            include 'right-sidebar.php';
+        ?>
+    </section>
+</div>
