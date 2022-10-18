5bec956502ae3fe42567f92a7bf0bafdfb087557
Dinmukhammed Tleuzhanuly <tdimasg@gmail.com>
2022-10-18 16:32:37 +0600

    part home

--- pages/home.php
+++ pages/home.php
@@ -1,21 +1,51 @@
 <div class="news">
-    <div class="news__header">
-        popular image
-    </div>
-    <div class="news__body">
-        <div class="news__creator-avatar">
+    <div class="news__item">
+        <div class="news__header">
+            Popular images · See more
         </div>
-        <div class="news__content">
-            <div class="content__header">
-                <div class="content__title">wild TikTok screenshots</div>
-                <div class="content__option"><i class="fa-solid fa-ellipsis"></i></div>
+        <div class="news__body">
+            <div class="news__creator-avatar">
             </div>
-            <div class="content__image">
+            <div class="news__content">
+                <div class="content__header">
+                    <div class="content__title">wild TikTok screenshots</div>
+                    <div class="content__option"><i class="fa-solid fa-ellipsis"></i></div>
+                </div>
+                <div class="content__image">
 
+                </div>
             </div>
         </div>
+        <div class="news__footer">
+            <div class="footer__item"><i class="fa-regular fa-comment"></i> 32</div>
+            <div class="footer__item"><i class="fa-regular fa-circle-down"></i> 23</div>
+            <div class="footer__item"><i class="fa-regular fa-heart"></i> 45</div>
+            <div class="footer__item"><i class="fa-regular fa-share"></i> 54</div>
+        </div>
     </div>
-    <div class="news__header">
-        buttons
+    <div class="news__item">
+        <div class="news__header">
+            Popular images · See more
+        </div>
+        <div class="news__body">
+            <div class="news__creator-avatar">
+            </div>
+            <div class="news__content">
+                <div class="content__header">
+                    <div class="content__title">wild TikTok screenshots</div>
+                    <div class="content__option"><i class="fa-solid fa-ellipsis"></i></div>
+                </div>
+                <div class="content__image">
+
+                </div>
+            </div>
+        </div>
+        <div class="news__footer">
+            <div class="footer__item"><i class="fa-regular fa-comment"></i> 32</div>
+            <div class="footer__item"><i class="fa-regular fa-circle-down"></i> 23</div>
+            <div class="footer__item"><i class="fa-regular fa-heart"></i> 45</div>
+            <div class="footer__item"><i class="fa-regular fa-share"></i> 54</div>
+        </div>
     </div>
+
 </div>
