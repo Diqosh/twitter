4c2bec8cc10ce96e877b97dae04c32022d5a7e66
Dinmukhammed Tleuzhanuly <tdimasg@gmail.com>
2022-11-30 17:04:03 +0600

    back auth

---
+++ api/auth/signin.php
@@ -0,0 +1,28 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/twitter/config/all.php";
+
+if(validate($_POST["login"], $_POST["password"])){
+
+    $login =$_POST['login'];
+    $password = $_POST['password'];
+    $hash = sha1($password);
+
+    $prep = mysqli_prepare($connect, "select * from users where login=? and password=?");
+    mysqli_stmt_bind_param($prep, "ss", $login, $hash);
+
+    mysqli_stmt_execute($prep);
+    $query = mysqli_stmt_get_result($prep);
+    if(mysqli_num_rows($query) != 1){
+        header("Location: ".BASE_URL.'/pages/register.php?error=4');
+        exit();
+    }
+    $row = mysqli_fetch_assoc($query);
+    session_start();
+    $_SESSION["user_id"] = $row["id"];
+    $_SESSION["login"] = $row["login"];
+
+    header("Location: ".BASE_URL."/index.php");
+
+}else {
+    header("Location: " . BASE_URL . '/pages/register.php?error=3');
+}
